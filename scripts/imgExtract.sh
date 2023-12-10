39b052a4de382b10c65c9d74a04ea6efde923908
Bobev <68569762+Bobev64@users.noreply.github.com>
2023-12-09 19:05:55 -0500

    Added distinction in plotting object classes

--- scripts/imgExtract.sh
+++ scripts/imgExtract.sh
@@ -2,36 +2,36 @@
 
 # USAGE: ./image_extract.sh [file_name] [number of images to sample (optional)]
 #if [[ "$1"=="*/" ]]; then
-find "$1" -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" \) -exec sh -c '
-	file=${1##*/}
-	fileName=${file%%.*}
-	echo "$file"
-	echo "$fileName"
-' find-sh {} \;
+# find "$1" -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.webm" \) -exec sh -c '
+# 	file=${1##*/}
+# 	fileName=${file%%.*}
+# 	echo "$file"
+# 	echo "$fileName"
+# ' find-sh {} \;
 
 # Get filename without extension
-# # Default num of images to extract is 30 if no argument provided
-# if [[ "$2" -eq "" ]]; then
-# 	imgCount=30
-# else
-# 	imgCount="$2"
-# fi
-# 
-# 
-# # Make folder to dump images to if it doesn't exist
-# destinationDir=./"$fileName"_imgs
-# mkdir -p $destinationDir
-# 
-# # -count_packets is way faster than -count_frames, although it *technically* doesn't count frames
-# totalFrames="$(ffprobe -v error -select_streams v:0 -count_packets \
-# 				-show_entries stream=nb_read_packets -of csv=p=0 $1)"
-# 
-# echo "TOTAL FRAME COUNT: $totalFrames"
-# 
-# # Calc num of frames to step by for desired image count
-# # NOTE: Occasionally outputs one more frame than desired.
-# frameStep=$((totalFrames / imgCount))
-# 
-# # Output frames from video with frame number
-# ffmpeg -i "$1" -vf "select=not(mod(n\,$frameStep))" -fps_mode \
-# 				passthrough "${destinationDir}/${fileName}_frame_%d.jpg"
+# Default num of images to extract is 30 if no argument provided
+if [[ "$2" -eq "" ]]; then
+	imgCount=30
+else
+	imgCount="$2"
+fi
+
+
+# Make folder to dump images to if it doesn't exist
+destinationDir=./"$fileName"_imgs
+mkdir -p $destinationDir
+
+# -count_packets is way faster than -count_frames, although it *technically* doesn't count frames
+totalFrames="$(ffprobe -v error -select_streams v:0 -count_packets \
+				-show_entries stream=nb_read_packets -of csv=p=0 $1)"
+
+echo "TOTAL FRAME COUNT: $totalFrames"
+
+# Calc num of frames to step by for desired image count
+# NOTE: Occasionally outputs one more frame than desired.
+frameStep=$((totalFrames / imgCount))
+
+# Output frames from video with frame number
+ffmpeg -i "$1" -vf "select=not(mod(n\,$frameStep))" -fps_mode \
+				passthrough "${destinationDir}/${fileName}_frame_%d.jpg"
--- src/robotDetect.py
+++ src/robotDetect.py
@@ -3,9 +3,11 @@ from ultralytics import YOLO
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO: Replace all variable assignments of None with robust path assignments
-
+# TODO: Replace applicable variable assignments of None with robust path assignments
 mode = 2
+
+RED_ROBOT = 0.0
+BLUE_ROBOT = 1.0
 
 # Load the YOLOv8 model
 modelPath = None
@@ -13,26 +15,21 @@ model = YOLO(modelPath)
 
 # Open the video file
 video_path = None
-cap = cv2.VideoCapture(video_path)
+video = cv2.VideoCapture(video_path)
 
-# videoOut = None
-# video = cv2.VideoWriter(videoOut,cv2.VideoWriter_fourcc(*'MPEG'),30,(1080,1920))
-
-if mode == 1:
-    # Loop through the video frames
+def outputInferenceVid(cap):
+    # Loop through video frames
     while (True):
         # Read a frame from the video, return whether a frame can be read
         success, frame = cap.read()
     
         if success:
             # Run YOLOv8 inference on the frame
-            results = model(frame)
+            results = model.predict(frame, conf=0.5)
     
             # Visualize the results on the frame
             annotated_frame = results[0].plot()
     
-            # write the annotated frame to mp4 file
-            # video.write(annotated_frame)
             cv2.imshow("output", annotated_frame)
         # Press the key "q" to quit program
         if cv2.waitKey(1) == ord('q'):
@@ -40,33 +37,59 @@ if mode == 1:
     cv2.destroyAllWindows()
     # video.release()
 
-# Run through all frames, make heat map.
-elif mode == 2:
+def outputCenterPtPlot(cap):
+
+    imgPath = None
+    fig, ax = plt.subplots()
+    ax.imshow(plt.imread(imgPath))
+
     i = 0
-
-
     while(True):
         success, frame = cap.read()
         i += 1
-
         if success:
-
             results=model(frame)
+
+            # Retrieve x & y coordinates for opposing corners of all detected objects and convert them to a numpy array stored in the CPU
             npBoxArray = results[0].boxes.xyxy.cpu().numpy()
 
+            # Retrieve alliances of robots present
+            objClasses = results[0].boxes.cls.cpu().numpy()
+
+            # Only process frame if there's actual detection going on
             if npBoxArray.size != 0:
-                print(f"[+] Frame number: {i}\n[+] Results: {npBoxArray}\n")
+                print(f"[+] Frame number: {i}\n[+] Results: {npBoxArray}\n[+] Classes: {objClasses}\n")
+
+                # Get the bottom right corner of detected robot bounding box, plot on graph
                 for box in npBoxArray:
-                    mdptX = np.divide(np.add(box[0], box[2]), 2.0)
-                    mdptY = np.divide(np.add(box[1], box[3]), 2.0)
-                    print(f"\t[+] Midpoint X: {mdptX}\tMidpoint Y: {mdptY}")
-                    plt.plot(mdptX, mdptY, 'r,')
+                        ax.plot(box[2], box[3], 'r,')
+
+                    # NOTE: Enable this later when distinction between red & blue alliance bots has become more accurate.
+                    # for obj in objClasses:
+                        # mdptX = np.divide(np.add(box[0], box[2]), 2.0)
+                        # mdptY = np.divide(np.add(box[1], box[3]), 2.0)
+                        # print(f"\t[+] Midpoint X: {mdptX}\tMidpoint Y: {mdptY}")
+
+                        # Plot the midpoint as a red pixel or blue pixel depending on robot type
+                        # if obj == RED_ROBOT:
+                        #     ax.plot(box[2], box[3], 'r,')
+                        # elif obj == BLUE_ROBOT:
+                        #     ax.plot(box[2], box[3], 'b,')
+                        # else:
+                        #     print(f"[+] ERROR: Object class detected as: {obj}")
             else:
                 print("[+] No robots detected")
 
+        # If you can find the next frame, continue with loop
         if cap.grab() is True:
             continue
         else:
-            plt.show()
+            break
 
-cap.release()
+    cap.release()
+    plt.show()
+   
+if mode == 1:
+    outputInferenceVid(video) 
+elif mode == 2:
+    outputCenterPtPlot(video)
